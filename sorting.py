030ce0c795d9eec1e0c1604bdc28af6a6d778b83
hrashk <hrashk@yandex.ru>
2018-11-30 13:24:36 +0300

    edge cases

--- sorting.py
+++ sorting.py
@@ -1,3 +1,6 @@
 def partition(lst, pivot, left, right):
+    if len(lst) == 1:
+        return 0
+
     lst[0], lst[2] = lst[2], lst[0]
     return 2
--- tests.py
+++ tests.py
@@ -1,12 +1,23 @@
+import pytest
 from sorting import partition
 
 
 def test_basic():
     lst = [4, 3, 1, 7, 13, 9, 23]
-    pivot_idx = 0
-    pivot = lst[pivot_idx]  # == 4
-    idx = partition(lst, pivot_idx, 0, len(lst) - 1)
+    pivot = lst[0]  # == 4
+    idx = partition(lst, pivot, 0, len(lst) - 1)
     assert idx == 2
     assert lst[idx] == pivot
     assert all([lst[i] < pivot for i in range(idx)])
     assert all([lst[i] > pivot for i in range(idx + 1, len(lst))])
+
+
+def test_edges():
+    assert partition([1], 0, 0, 0) == 0
+
+    with pytest.raises(IndexError):
+        partition([], 0, 0, 1)
+    with pytest.raises(IndexError):
+        partition([1, 2], 0, -3, 1)
+    with pytest.raises(IndexError):
+        partition([1, 2], 0, 0, 2)
