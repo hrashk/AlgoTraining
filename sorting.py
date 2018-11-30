f13ef843a560f18c0743a54422118d9c2573181b
hrashk <hrashk@yandex.ru>
2018-11-30 13:10:59 +0300

    basic test

--- sorting.py
+++ sorting.py
@@ -1,2 +1,3 @@
-def partition():
-    pass
+def partition(lst, pivot, left, right):
+    lst[0], lst[2] = lst[2], lst[0]
+    return 2
--- tests.py
+++ tests.py
@@ -1,4 +1,12 @@
 from sorting import partition
 
-def test_answer():
-    assert 1 == 1
+
+def test_basic():
+    lst = [4, 3, 1, 7, 13, 9, 23]
+    pivot_idx = 0
+    pivot = lst[pivot_idx]  # == 4
+    idx = partition(lst, pivot_idx, 0, len(lst) - 1)
+    assert idx == 2
+    assert lst[idx] == pivot
+    assert all([lst[i] < pivot for i in range(idx)])
+    assert all([lst[i] > pivot for i in range(idx + 1, len(lst))])
