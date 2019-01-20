30dddd15affb48c1ca9aec2d2f6c2a0ee3a05107
hrashk <hrashk@yandex.ru>
2019-01-20 16:23:56 +0300

    almost working!

--- decibinary.py
+++ decibinary.py
@@ -16,11 +16,11 @@ def precompute(maxrank):
         F.append([0])
 
         # find the minimum length of the decibinary representation
-        i = 1
+        L = 1
         pow2 = 2
         while n > 9 * (pow2 - 1):
             F[n].append(0)
-            i += 1
+            L += 1
             pow2 *= 2
         # recursive step
         while pow2 // 2 <= n:
@@ -29,26 +29,47 @@ def precompute(maxrank):
                 next_number = n - d * pow2 // 2
                 if next_number < 0:
                     break
-                F[n][i] += F[next_number][i - 1] if i - \
-                    1 < len(F[next_number]) else F[next_number][-1]
-            F[n][i] += F[n][i - 1]
-            i += 1
+                F[n][L] += getF(F, next_number, L)
+            F[n][L] += F[n][L - 1]
+            L += 1
             pow2 *= 2
         S.append(S[-1] + F[n][-1])
     return F, S
 
+def getF(F, n, L):
+    return F[n][L - 1] if L - 1 < len(F[n]) else F[n][-1]
+
 
 F, S = precompute(10**4)  # todo: repelace with 10**16
-# if __debug__:
-#     print("The biggest number:", len(F), " Last # stats ", F[-1][-1]," S[-1] =", S[-1])
-# for n, c in enumerate(F):
-#     print(n, '->', c)
+if __debug__:
+    #     print("The biggest number:", len(F), " Last # stats ", F[-1][-1]," S[-1] =", S[-1])
+    for n in range(12):
+        print(n, '->', S[n], '->', F[n])
 
 
 def decibinaryNumbers(x):
+    res = 0
+
     n = bisect_left(S, x)
-    i = bisect_left(F[n], x - S[n-1])
-    return n
+    if n == 0:
+        return 0
+    rank = x - S[n-1] # rank in the representations of n
+    L = bisect_left(F[n], rank)
+    rank -= getF(F, n, L) # rank in the representations of length L
+    while L > 1:
+        for d in range(1, 10):
+            next_number = n - d * 2**(L-1)
+            nreps = getF(F, next_number, L)
+            if rank <= nreps:
+                res += d * 10**(L-1)
+                n = next_number
+                break
+            else:
+                rank -= nreps
+        L = bisect_left(F[n], rank)
+        rank -= getF(F, n, L)
+    res += n
+    return res
 
 
 if __name__ == '__main__':
