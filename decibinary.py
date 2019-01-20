469901c6120922a956601e0f90fd02e208cac351
hrashk <hrashk@yandex.ru>
2019-01-20 14:36:33 +0300

    decreased memory usage by the precompute

--- decibinary.py
+++ decibinary.py
@@ -5,46 +5,51 @@ import re
 import sys
 from bisect import bisect_left
 
+
 def precompute(maxrank):
     # F[n][i] is the # of decibinary representations of n of length 0..i for i > 0
-    F = [[1] * 20] # sentinel
-    S = [1] # S[i] = sum(F[:i+1][-1])
-    n = 0 # the number to represent
+    F = [[1]]  # sentinel
+    S = [1]  # S[i] = sum(F[:i+1][-1])
+    n = 0  # the number to represent
     while S[-1] < maxrank:
         n += 1
-        F.append([0] * 20)
+        F.append([0])
 
         # find the minimum length of the decibinary representation
         i = 1
         pow2 = 2
         while n > 9 * (pow2 - 1):
+            F[n].append(0)
             i += 1
             pow2 *= 2
         # recursive step
         while pow2 // 2 <= n:
+            F[n].append(0)
             for d in range(1, 10):
                 next_number = n - d * pow2 // 2
                 if next_number < 0:
                     break
-                F[n][i] += F[next_number][i - 1]
+                F[n][i] += F[next_number][i - 1] if i - \
+                    1 < len(F[next_number]) else F[next_number][-1]
             F[n][i] += F[n][i - 1]
             i += 1
             pow2 *= 2
-        while i < len(F[n]):
-            F[n][i] = F[n][i - 1]
-            i += 1
         S.append(S[-1] + F[n][-1])
     return F, S
 
-F, S = precompute(10**5) # todo: repelace with 10**16
+
+F, S = precompute(10**4)  # todo: repelace with 10**16
 # if __debug__:
 #     print("The biggest number:", len(F), " Last # stats ", F[-1][-1]," S[-1] =", S[-1])
-#     for n, c in enumerate(F):
-#         print(n, '->', c)
+# for n, c in enumerate(F):
+#     print(n, '->', c)
+
 
 def decibinaryNumbers(x):
     n = bisect_left(S, x)
+    i = bisect_left(F[n], x - S[n-1])
     return n
+
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
@@ -56,4 +61,3 @@ if __name__ == '__main__':
             result = decibinaryNumbers(x)
 
             print(str(result))
-
