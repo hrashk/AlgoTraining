8da4c9a361859ff72872807051c63897564c3753
hrashk <hrashk@yandex.ru>
2019-04-11 17:45:00 +0300

    some progress

--- balanced-forest.py
+++ balanced-forest.py
@@ -32,19 +32,49 @@ def dfs_augment_weight(graph, c, start):
     return weights, parent
 
 def dfs_cut(graph, weights, parent, total):
-    candidates1 = []
-    candidates2 = []
+    def dfs_up(v):
+        return False
+    
+    def dfs_down(v):
+        ref_weight = total - weights[v]
+        visited = set((v,))
+        stack = [v]
+        while len(stack) > 0:
+            top = stack[-1]
+            unvisited = [u for u in graph[top] if u not in visited]
+
+            if len(unvisited) == 0:
+                stack.pop()
+            else:
+                stack.extend(unvisited)
+                visited.update(unvisited)
+                
+                for u in unvisited:
+                    if weights[u] == ref_weight or weights[v] - weights[u] == ref_weight:
+                        return True
+        return False
+
+    candidates_up = []
+    candidates_down = []
 
     for v, dad in parent.items():
         if 3 * weights[v] > total and 2 * weights[v] < total:
-            candidates1.append(v)
+            candidates_up.append(v)
         if 3 * (total - weights[v]) > total and 2 * (total - weights[v]) < total:
-            candidates2.append(v)
+            candidates_down.append(v)
 
-    print("candidates1:", candidates1)
-    print("candidates2:", candidates2)
+    print("candidates_up:", candidates_up)
+    print("candidates_down:", candidates_down)
 
-    return 0
+    min_up = min((total - 2 * weights[v] for v in candidates_up if dfs_up(v)), default=-1)
+    min_down = min((total - 2 * weights[v] for v in candidates_down if dfs_down(v)), default=-1)
+    
+    if min_up == -1:
+        return min_down
+    elif min_down == -1:
+        return min_up
+    else:
+        return min(min_up, min_down)
 
 # Complete the balancedForest function below.
 def balancedForest(c, edges):
